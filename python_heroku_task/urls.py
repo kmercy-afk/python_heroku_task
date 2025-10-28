4b0f5f8addd9c8601e21c199c326daacf1ffa5a1
kmercy119@gmail.com <kmercy119@gmail.com>
2025-10-28 12:16:21 +0300

    Added root redirect to blog

--- python_heroku_task/urls.py
+++ python_heroku_task/urls.py
@@ -1,7 +1,26 @@
+"""python_heroku_task URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from my_app import urls as my_app_urls
+    2. Add a URL to urlpatterns:  path('my_app/', include(my_app_urls))
+Note that the order matters: broader patterns should come first, more specific later.
+"""
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect  # New: For redirecting root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),  # Your blog paths
+    path('', lambda request: HttpResponseRedirect('/blog/')),  # New: Redirect root to blog
 ]
